e6941c0bae100ea2de075bdab24d124e573b4bc1
Hakim Boulahya <hakimblhya@gmail.com>
2017-12-10 17:04:44 +0100

    QLearning algo impl (in dev)

--- nbandits.py
+++ nbandits.py
@@ -3,13 +3,20 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import logging
 import os
+import random
 import sys
 
 # Specify backend, to allow usage from terminal
 plt.switch_backend('agg')
 # Logger
 log = logging.getLogger(__name__)
-logging.basicConfig(level = logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
+
+
+class SimulationException(Exception):
+    pass
+
+
 class utils:
     @staticmethod
     def mkdir(directory):
@@ -32,9 +39,9 @@ class utils:
             sys.exit(1)
 
     @staticmethod
-    def plot(fig, data, axis, xlabel, ylabel, message = None):
+    def plot(fig, data, axis, xlabel, ylabel, message=None):
         if message is None:
-            message = "Plot x: %s; y:%s" %(xlabel, ylabel)
+            message = "Plot x: %s; y:%s" % (xlabel, ylabel)
         log.info("%s in '%s'" % (message, fig))
         plt.axis(axis)
         plt.xlabel(xlabel)
@@ -43,25 +50,67 @@ class utils:
         plt.savefig(fig, bbox_inches='tight')
         plt.close()
 
+    @staticmethod
+    def normal(mu, sigma):
+        return np.random.normal(mu, sigma, 20)
+
+
 class Simulation:
     def __init__(self, config):
         self.config = config
+        self.Q = [None for i in range(self.n)]
+
+    @property
+    def n(self):
+        """
+        :return: number of actions
+        """
+        return len(self.config['qa_opt'])
+
+    @property
+    def alpha(self):
+        return 0.9
+
+    def random_action(self):
+        return random.randint(0, self.n - 1)
+
+    def choose_action(self, t):
+        method = self.config['action_select_method']
+        if method == 'random':
+            return self.random_action()
+        else:
+            raise SimulationException(
+                "Unknown action selection method '%s'" % method)
+
+    def initialize_q(self):
+        self.Q = [self.config['qa_init'] for i in range(self.n)]
+
+    def q_opt(self, action):
+        return self.config['qa_opt'][action]
+
+    def sigma(self, action):
+        return self.config['sigma'][action]
+
+    def reward(self, action, t):
+        return utils.normal(self.q_opt(action), self.sigma(action))
+
+    def update_q(self, action, t):
+        self.Q[action] = self.Q[action] + self.alpha + self.reward(action, t)
 
     def q_learning(self):
         self.initialize_q()
         for t in range(self.config['time_steps']):
-            self.choose_action()
-            self.update_q()
-
+            action = self.choose_action(t)
+            self.update_q(action, t)
 
     def run(self):
         log.info("Running simulation")
         self.q_learning()
         log.info("Simulation finished")
 
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Error: config file not given.")
         sys.exit(1)
-    config = utils.get_config(sys.argv[1])
-    Simulation(config).run()
+    Simulation(utils.get_config(sys.argv[1])).run()
--- sample.py
+++ sample.py
@@ -1,14 +1,18 @@
 number_of_simulations = 1
 
-time_steps = 1000
+time_steps = 10
 
 qa_init = 0
 
-sigma = [0.9, 0.6, 0.4]
+# Action Table 3
+sigma = [0.9, 0.6, 0.4, 2]
+# Qa*
 qa_opt = [1.3, 1.1, 0.5, 0.3]
-
+# Action selection methods: {'random' 'e_greedy' 'softmax'}
+action_select_method = 'random'
 
 # Can depend on t, this is why it's a string
 # e-greedy = '1/(t**(1/2))' # 1/sqrt(t)
 e_greedy = '1'
 tau = '4* ( (1000 - t) / 1000 )'
+test = 't+2'
